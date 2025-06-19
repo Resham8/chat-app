46f57172be561bdeb26d275b449f01243b45dddd
Resham <reshamupadhyay48@gmail.com>
2025-06-19 22:32:13 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,40 @@ const wss = new WebSocketServer({ port: 8080 });
 interface User {
   socket: WebSocket;
   room: string;
+  username:string;
 }
 
 let allSockets: User[] = [];
+
+function boadcastRoomInfo(room: string) {
+  const usersInRoom = allSockets.filter((u) => u.room === room);
+  const onlineCount = usersInRoom.length;
+
+  usersInRoom.forEach((user) => {
+    user.socket.send(
+      JSON.stringify({
+        type: "roomInfo",
+        payload: {
+          roomId: room,
+          onlineCount,
+        },
+      })
+    );
+  });
+}
 
 wss.on("connection", (socket) => {
   socket.on("message", (message) => {
     const parsedMessage = JSON.parse(message as unknown as string);
     if (parsedMessage.type === "join") {
+      const { roomId, username } = parsedMessage.payload;
       allSockets.push({
         socket,
-        room: parsedMessage.payload.roomId,
+        room: roomId,
+        username,
       });
+
+      boadcastRoomInfo(roomId);
     }
 
     if (parsedMessage.type === "chat") {
@@ -28,10 +50,25 @@ wss.on("connection", (socket) => {
       allSockets
         .filter((user) => user.room === currentUserRoom)
         .forEach((user) => {
-          user.socket.send(parsedMessage.payload.message);
+          user.socket.send(
+            JSON.stringify({
+              type: "chat",
+              payload: {
+                message: parsedMessage.payload.message,
+                username: currentUser.username,
+              },
+            })
+          );
         });
     }
   });
 
-  socket.on("disconnect", () => {});
+  socket.on("close", () => {
+    const user = allSockets.find((u) => u.socket === socket);
+    if (!user) return;
+
+    allSockets = allSockets.filter((u) => u.socket !== socket);
+
+    boadcastRoomInfo(user.room);
+  });
 });
